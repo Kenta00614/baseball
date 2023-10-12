9d94c889c0749ae9ea2f9826196cb1f596f3061c
n_ikari <n_ikari@10.202.28.40>
2023-10-12 12:12:29 +0900

    1

--- baseball/src/baseball/test.java
+++ baseball/src/baseball/test.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class test extends HttpServlet{
-	public void doGet(
+	public void doPost(
 			HttpServletRequest request, HttpServletResponse response
 		)throws ServletException, IOException {
 	}
