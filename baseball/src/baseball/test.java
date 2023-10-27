5f4b46c1987b8634ca52dc8d743722a2c7608550
K_Matsumoto <aru84ken.popo@gmail.com>
2023-10-27 14:50:53 +0900

    test

--- baseball/src/baseball/test.java
+++ baseball/src/baseball/test.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class test extends HttpServlet{
-	public void doPost(
+	public void doGet(
 			HttpServletRequest request, HttpServletResponse response
 		)throws ServletException, IOException {
 	}
