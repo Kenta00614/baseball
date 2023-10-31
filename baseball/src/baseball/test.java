03b161e8eb979f54e7d999d2f7402e01841b1a6a
K_Matsumoto <aru84ken.popo@gmail.com>
2023-10-31 09:51:07 +0900

    test

--- baseball/src/baseball/test.java
+++ baseball/src/baseball/test.java
@@ -7,9 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class test extends HttpServlet{
-	public void doPost(
-			HttpServletRequest request, HttpServletResponse response
-		)throws ServletException, IOException {
-	}
+public class test extends HttpServlet {
+    public void doPost(
+        HttpServletRequest request, HttpServletResponse response
+    ) throws ServletException, IOException {
+        response.setContentType("text/plain"); // レスポンスのコンテンツタイプを設定
+        response.setCharacterEncoding("UTF-8"); // 文字エンコーディングを設定
+
+        // レスポンスに「こんにちは」というテキストを書き込む
+        response.getWriter().write("こんにちは");
+    }
 }
