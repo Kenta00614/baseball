2aae3d9526d6c70d2efb3ef7dfecfe1001635864
s_ono <s_ono@10.202.25.191>
2023-11-02 12:44:08 +0900

    Duel.java

---
+++ baseball/src/bean/Duel.java
@@ -0,0 +1,11 @@
+package bean;
+
+import java.io.Serializable;
+
+public class Duel implements Serializable {
+	private int duelId;
+	private int school1;
+	private int school2;
+	private int school3;
+
+}
