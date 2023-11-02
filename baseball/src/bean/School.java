b69ee627d513d74d8458a2f7e731af0d25bd0480
s_ono <s_ono@10.202.25.191>
2023-11-02 13:54:07 +0900

    school.java

---
+++ baseball/src/bean/School.java
@@ -0,0 +1,28 @@
+package bean;
+
+import java.io.Serializable;
+
+public class School implements Serializable {
+	private int schoolId;
+	private int tournamentId;
+	private String name;
+
+	public int getSchoolId() {
+		return schoolId;
+	}
+	public void setSchoolId(int schoolId) {
+		this.schoolId = schoolId;
+	}
+	public int getTournamentId() {
+		return tournamentId;
+	}
+	public void setTournamentId(int tournamentId) {
+		this.tournamentId = tournamentId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
