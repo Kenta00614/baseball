122007625d1ddfc5d6d49cedb49a4b3deb658466
milktea1008 <m39471@icloud.com>
2023-11-02 16:21:18 +0900

    a

---
+++ baseball/WebContent/staff/staffRegister.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@include file="header.jsp"%>
+<html>
+
+<body>
+    <div class="header">
+    <h1>新規職員登録</h1>
+        <form action="StaffRegisterComplete" method="get">
+    　　　　<button type="submit">登録</button>
+　　　　</form>
+    </div>
+
+</body>
+</html>
--- baseball/src/staff/StaffDelete.java
+++ baseball/src/staff/StaffDelete.java
@@ -8,11 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/StaffList")
-public class StaffList extends HttpServlet {
+@WebServlet("/staff/StaffDelete")
+public class StaffDelete extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        request.getRequestDispatcher("/staff/staffList.jsp").forward(request, response);
+        request.getRequestDispatcher("/staff/staffDelete.jsp").forward(request, response);
     }
 }
