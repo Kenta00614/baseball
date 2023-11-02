d0ea92bdefa99446e30aa6ccc5544704dbe94799
s_ono <s_ono@10.202.25.191>
2023-11-02 13:51:11 +0900

    a

--- baseball/src/bean/Duel.java
+++ baseball/src/bean/Duel.java
@@ -6,6 +6,30 @@ public class Duel implements Serializable {
 	private int duelId;
 	private int school1;
 	private int school2;
-	private int school3;
+	private int status;
 
+	public int getDuelId() {
+		return duelId;
+	}
+	public void setDuelId(int duelId) {
+		this.duelId = duelId;
+	}
+	public int getSchool1() {
+		return school1;
+	}
+	public void setSchool1(int school1) {
+		this.school1 = school1;
+	}
+	public int getSchool2() {
+		return school2;
+	}
+	public void setSchool2(int school2) {
+		this.school2 = school2;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
 }
