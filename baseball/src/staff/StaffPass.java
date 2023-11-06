012489df55a96b163376ba9708d1059fe079ff01
milktea1008 <m39471@icloud.com>
2023-11-06 14:31:01 +0900

    あ

--- baseball/src/staff/StaffPass.java
+++ baseball/src/staff/StaffPass.java
@@ -8,11 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/staff/StaffRegisterComplete")
-public class StaffRegisterComplete extends HttpServlet {
+@WebServlet("/staff/StaffPass")
+public class StaffPass extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        request.getRequestDispatcher("/staff/staffRegisterComplete.jsp").forward(request, response);
+        request.getRequestDispatcher("/staff/staffPass.jsp").forward(request, response);
     }
 }
