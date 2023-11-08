5ce785474b5352bc18f7f1d35db052b51acb4db2
n_ikari <n_ikari@10.202.28.40>
2023-11-08 12:52:01 +0900

    getMatchAll()の作成

--- baseball/src/ikari/Test.java
+++ baseball/src/ikari/Test.java
@@ -2,6 +2,7 @@ package ikari;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,7 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import bean.Duel;
-import dao.DuelDAO;
+import bean.Match;
+import dao.MatchDAO;
 
 @WebServlet("/test")
 public class Test extends HttpServlet{
@@ -33,27 +35,27 @@ public class Test extends HttpServlet{
 		out.println("<body>");
 
 		try{
-			/*
-			 Match mm=new Match();
-			mm.setTournamentId(2);
-			mm.setEventDate(Date.valueOf("2023-10-25"));
-			mm.setSaleStartAt(Date.valueOf("2023-10-26"));
-			mm.setDuel1(9);
-			mm.setDuel2(10);
-			mm.setDuel3(11);
-			mm.setDuel4(12);
-			*/
+
+			Match mm=new Match();
+			mm.setMatchId(2);
+			mm.setTournamentId(1);
+			mm.setEventDate(Date.valueOf("2023-10-24"));
+			mm.setSaleStartAt(Date.valueOf("2023-10-25"));
+			mm.setDuel1(8);
+			mm.setDuel2(7);
+			mm.setDuel3(6);
+			mm.setDuel4(5);
+
 
 			Duel m=new Duel();
 			m.setDuelId(1);
-			m.setSchool1(1);
-			m.setSchool2(2);
-			m.setStatus("2");
-			m.setRound("2");
+			m.setSchool1(2);
+			m.setSchool2(1);
+			m.setStatus("3");
+			m.setRound("3");
 
-
-			DuelDAO D = new DuelDAO();
-		int num= D.changeDuel(m);
+			MatchDAO D = new MatchDAO();
+			int num=D.changeMatch(mm, m);
 
 			out.println(num);
 
