74925e848e0262c96bfd6394f7f160a9f58544bc
K_Matsumoto <aru84ken.popo@gmail.com>
2023-11-14 11:14:34 +0900

    staffのログイン

--- baseball/WebContent/staff/header.jsp
+++ baseball/WebContent/staff/header.jsp
@@ -12,7 +12,12 @@
     <header class="staff-header">
     	<div class="staff__wrapper">
 			<ul class="nav-menu">
-			    <li class="nav__item">
+
+			<%
+                if (session.getAttribute("staff") != null) {
+                    // ログインしている時のメニュー
+            %>
+            　　<li class="nav__item">
 			        <a href="#">試合情報 &#9662;</a> <!-- ドロップダウンメニューのトリガー -->
 			        <div class="dropdown">
 						<a href="">試合情報登録</a>
@@ -26,8 +31,30 @@
 				<li class="nav__item"><a href="StaffList">職員情報</a></li>
 				<li class="nav__item"><a href="EntryExit">入退場</a></li>
 				<li class="nav__item"><a href="Logout">ログアウト</a></li>
+            <%
+                } else {
+                    // ログインしていない時のメニュー
+            %>
+　　　　　　            　　<li class="nav__item">
+			        <a href="#">試合情報 &#9662;</a> <!-- ドロップダウンメニューのトリガー -->
+			        <div class="dropdown">
+						<a href="">試合情報登録</a>
+						<a href="">試合情報表示</a>
+						<a href="">大会情報登録</a>
+				    </div>
+				</li>
+			    <li class="nav__item"><a href="HighschoolList">高校情報</a></li>
+			    <li class="nav__item"><a href="SaleStop">販売停止</a></li>
+			    <li class="nav__item"><a href="Refund">払い戻し</a></li>
+				<li class="nav__item"><a href="StaffList">職員情報</a></li>
+				<li class="nav__item"><a href="EntryExit">入退場</a></li>
+				<li class="nav__item"><a href="LoginDisplay">ログイン</a></li>
 
+            <%
+                }
+            %>
 			</ul>
+
 
 		</div>
 		<hr>
--- baseball/src/staff/Login.java
+++ baseball/src/staff/Login.java
@@ -7,12 +7,36 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bean.Staff;
+import dao.StaffDAO;
 @WebServlet("/staff/Login")
 public class Login extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String id = request.getParameter("id");
+        String password = request.getParameter("password");
 
-        request.getRequestDispatcher("/staff/login.jsp").forward(request, response);
+
+        StaffDAO staffDAO = new StaffDAO();
+
+        try {
+        	Staff staff = staffDAO.loginStaff(id, password);
+            if (staff != null) {
+                // ログイン成功
+                HttpSession session = request.getSession();
+                session.setAttribute("staff", staff);
+                response.sendRedirect("/baseball/staff/loginWelcome.jsp"); // ログイン成功ページへリダイレクト
+            } else {
+                // ログイン失敗
+                HttpSession session = request.getSession();
+                session.setAttribute("loginError", "無効なメールアドレスまたはパスワードです。");
+                response.sendRedirect("/baseball/staff/loginError.jsp"); // ログインページへリダイレクト
+            }
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
     }
 }
