7c802801b1195f5a7989e4d5ee405400786c71fb
n_ikari <n_ikari@10.202.28.40>
2023-11-16 10:48:06 +0900

    updateMail()の作成

--- baseball/src/ikari/Test.java
+++ baseball/src/ikari/Test.java
@@ -2,15 +2,11 @@ package ikari;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.UUID;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import bean.Provisional;
-import dao.ProvisionalDAO;
 
 @WebServlet("/test")
 public class Test extends HttpServlet{
@@ -35,19 +31,7 @@ public class Test extends HttpServlet{
 
 		try{
 
-			Provisional p=new Provisional();
-			ProvisionalDAO MD=new ProvisionalDAO();
 
-			String uuid = "22ca95f0-d0ba-4e33-bd1d-46913e903c5c";
-			UUID newUuid=UUID.fromString(uuid);
-
-			p=MD.getIdAndNewMail(newUuid);
-
-			int Id=p.getSpectatorId();
-			String mail=p.getMail();
-
-			System.out.println(Id);
-			System.out.println(mail);
 
 		}catch(Exception e){
 			e.printStackTrace(out);
