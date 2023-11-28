bea0bd937ad84ff491196f79c17a73d779b80e33
K_Matsumoto <aru84ken.popo@gmail.com>
2023-11-28 10:05:22 +0900

    高校登録

--- baseball/src/staff/HighschoolRegistrationDisplay.java
+++ baseball/src/staff/HighschoolRegistrationDisplay.java
@@ -1,7 +1,6 @@
 package staff;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,27 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import bean.School;
-import dao.SchoolDAO;
-
 @WebServlet("/staff/HighschoolRegistrationDisplay")
 public class HighschoolRegistrationDisplay extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String tournamentIdStr = request.getParameter("tournamentId");
+        // 大会IDをリクエスト属性にセット
+        String tournamentId = request.getParameter("tournamentId");
+        request.setAttribute("tournamentId", tournamentId);
 
-        if (tournamentIdStr != null && !tournamentIdStr.isEmpty()) {
-            int tournamentId = Integer.parseInt(tournamentIdStr);
-            SchoolDAO dao = new SchoolDAO();
-
-            try {
-                List<School> schools = dao.searchSchool(tournamentId);
-                request.setAttribute("schools", schools);
-                request.setAttribute("tournamentId", tournamentId);
-                request.getRequestDispatcher("/staff/highschoolDisplay.jsp").forward(request, response);
-            } catch (Exception e) {
-                throw new ServletException(e);
-            }
-        }
+        request.getRequestDispatcher("/staff/highschoolRegistration.jsp").forward(request, response);
     }
 }
