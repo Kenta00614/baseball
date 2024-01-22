c4df5edc15fd93a400bfb4b107d6f6ac99f78381
s_ono <s_ono@10.202.25.191>
2024-01-22 15:53:10 +0900

    エラー修正

--- baseball/src/customer/TicketApplication.java
+++ baseball/src/customer/TicketApplication.java
@@ -12,14 +12,17 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import bean.Match;
+import bean.Tournament;
 import common.Constants;
 import dao.MatchDAO;
+import dao.TournamentDAO;
 
 @WebServlet("/customer/TicketApplication")
 public class TicketApplication extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session=request.getSession();
 		Match match = (Match) session.getAttribute("match");
+		Tournament tour = (Tournament) session.getAttribute("tour");
 		List<String> seatType = new ArrayList<>();
 
 		String[] seatOrder = {"0B","0F","0T","0R","0L"};
@@ -39,6 +42,19 @@ public class TicketApplication extends HttpServlet {
     		 MatchDAO matchDAO=new MatchDAO();
     		 try {
     			match=matchDAO.getMatchInfo(matchId);
+
+    			if(tour == null){
+    				List<Tournament> list=new ArrayList<>();
+//    				大会情報取得
+    				TournamentDAO tourDao=new TournamentDAO();
+    				list=tourDao.getTournamentDetail();
+//    				最後の大会情報
+    				for(Tournament tournament: list){
+    					tour=tournament;
+    				}
+    				session.setAttribute("tour", tour);
+    			}
+
     		} catch (Exception e) {
     			e.printStackTrace();
     		}
