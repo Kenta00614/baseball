189610eece309415050f8e969ef4717e5388e21e
s_ono <s_ono@192.168.3.7>
2024-02-06 15:13:39 +0900

    uuid無いときはパスワード入力画面に行かないようにする

--- baseball/src/customer/PasswordChangeDisplay.java
+++ baseball/src/customer/PasswordChangeDisplay.java
@@ -1,6 +1,7 @@
 package customer;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,13 +9,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.Provisional;
+import dao.ProvisionalDAO;
+
 @WebServlet("/customer/PasswordChangeDisplay")
 public class PasswordChangeDisplay extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     	String uuidString = request.getParameter("uuid");
-    	request.setAttribute("uuid", uuidString);
-        request.getRequestDispatcher("/customer/passwordChange.jsp").forward(request, response);
+
+        UUID uuid = UUID.fromString(uuidString);
+        ProvisionalDAO provisionalDAO = new ProvisionalDAO();
+        Provisional seerchProv= null;
+        try {
+        	seerchProv = provisionalDAO.searchUuid(uuid);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+        if (seerchProv != null) {
+        	request.setAttribute("uuid", uuidString);
+            request.getRequestDispatcher("/customer/passwordChange.jsp").forward(request, response);
+            return;
+        } else {
+        	request.setAttribute("sucssesFlg", "1");
+        	request.getRequestDispatcher("/customer/passwordReset.jsp").forward(request, response);
+        }
     }
 }
